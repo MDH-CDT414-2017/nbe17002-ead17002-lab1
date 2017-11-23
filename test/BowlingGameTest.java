92732a49a0de6972cf69a979b464d6f22c0a5b6b
Deluvi <noe.baylac@protonmail.com>
2017-11-23 13:36:14 +0100

    Initial commit

---
+++ test/BowlingGameTest.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+	/** test01 
+	 * 	
+	 *  If no game is provided, score should be -1 (error)   
+	 */	     
+	public void test01() {
+        BowlingGame bowlingGame = new BowlingGame("");
+        assertEquals(-1, bowlingGame.getScore());
+    }	
+	
+	//TODO: Add more test cases below...
+	
+}
